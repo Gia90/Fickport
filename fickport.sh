54cf6d9b5832c43ddba1c72e9c243e421d9ac87c
Gia90 <Gia90@users.noreply.github.com>
2017-11-26 20:38:11 +0100

    Added pwd hashing and login

--- fickport.sh
+++ fickport.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 speedport=speedport.ip
-
+devpwd=***REMOVED***
 
 # Get the challenge value
 challenge=$(wget -q -O - "http://${speedport}/data/Login.json?_time=1511719881983&_rand=666&csrf_token=sercomm_csrf_token" | sed 's/.*challenge", "varvalue"\: "\([^"]*\).*/\1/')
 
-# Log in to the server.  This only needs to be done once.
+# Hashed password + challenge
+hashpwd=$(echo -n "${devpwd}${challenge}" | sha256sum)
+
+# Log in to the server
 wget --save-cookies cookies.txt \
      --keep-session-cookies \
-     --post-data 'user=foo&password=bar' \
+     --post-data 'password=${hashpwd}&showpw=0&csrf_token=sercomm_csrf_token' \
      --delete-after \
-     http://server.com/auth.php
+     "http://${speedport}//data/Login.json?lang=en"
+
+
