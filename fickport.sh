56f936fe506356575a4b4e0fdfe2368208b320dc
Gia90 <Gia90@users.noreply.github.com>
2017-12-01 00:16:38 +0100

    Fixed: single quotes not making variable expand
    Added more logging

--- fickport.sh
+++ fickport.sh
@@ -6,21 +6,33 @@ devpwd=***REMOVED***
 # Get the challenge value
 challenge=$(wget -q -O - "http://${speedport}/data/Login.json?_time=1511719881983&_rand=666&csrf_token=sercomm_csrf_token" | sed 's/.*challenge", "varvalue"\: "\([^"]*\).*/\1/')
 
+echo "[>] CHALLENGE: $challenge"
+
 # Hashed password + challenge
 hashpwd=$(echo -n "${devpwd}${challenge}" | sha256sum | cut -d" " -f1)
 
+echo "[>] HASHEDPWD: $hashpwd"
+
 # Log in to the server
-wget --save-cookies cookies.txt \
+echo "[>] LOG IN ..."
+wget -q \
+     --save-cookies cookies.txt \
      --keep-session-cookies \
-     --post-data 'password=${hashpwd}&showpw=0&csrf_token=sercomm_csrf_token' \
+     --post-data "password=${hashpwd}&showpw=0&csrf_token=sercomm_csrf_token" \
      --delete-after \
      "http://${speedport}/data/Login.json?lang=en"
+
+# TODO: check if correctly logged in
 
 # Extract CSRF Token
 csrftoken=$(wget --load-cookies cookies.txt -q -O - "http://${speedport}/html/content/overview/index.html?lang=en" | grep "var csrf_token " | cut -d "'" -f 2)
 
+echo "[>] CSRF-TOKEN: $csrftoken"
+
 # Reboot!
-wget --load-cookies cookies.txt \
-     --post-data 'reboot_device=true&csrf_token=${csrftoken}' \
+echo "[>] REBOOT!"
+wget -q \
+     --load-cookies cookies.txt \
+     --post-data "reboot_device=true&csrf_token=${csrftoken}" \
      --delete-after \
      "http://${speedport}/data/Reboot.json?_time=1511727315027&_rand=128&csrf_token=${csrftoken}&lang=en"
