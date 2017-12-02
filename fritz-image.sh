3c7b7ec2566c0600b6d069228443ee0a1297ddb7
Gia90 <Gia90@users.noreply.github.com>
2017-12-02 14:08:44 +0100

    Use devpwd.txt to store the device password to use with fickport

--- fritz-image.sh
+++ fritz-image.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 IMAGENAME="fickport.tar"
+SECRET=$(cat devpwd.txt)
 
 mkdir -p var/fickport/
 cp install var/
 cp *.sh var/fickport/
 cp -R bin var/fickport
+
+# Replace device password with the one defined in the "devpwd.txt" file
+sed -i "s/\?\{8\}/$SECRET/" var/fickport/fickport.sh
 
 tar -cf "$IMAGENAME" var/
 rm -R var
